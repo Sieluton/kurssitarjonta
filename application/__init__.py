f88d9e2786bc87785cd2ffd2e1b2c97e547739ca
Sami <sami.rinksam@gmail.com>
2018-09-16 04:59:16 +0300

    Viikon 2 projektin rakenne, tietokanta ja html sivut tehty

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,24 @@
+# Tuodaan Flask käyttöön
 from flask import Flask
 app = Flask(__name__)
 
+# Tuodaan SQLAlchemy käyttöön
+from flask_sqlalchemy import SQLAlchemy
+# Käytetään courses.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
+# kertoo, tiedosto sijaitsee tämän sovelluksen tiedostojen kanssa
+# samassa paikassa
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///courses.db"
+# Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
+app.config["SQLALCHEMY_ECHO"] = True
+
+# Luodaan db-olio, jota käytetään tietokannan käsittelyyn
+db = SQLAlchemy(app)
+
+# Luetaan kansiosta application tiedoston views sisältö
 from application import views
+
+from application.courses import models
+from application.courses import views
+
+# Luodaan lopulta tarvittavat tietokantataulut
+db.create_all()
---
+++ application/courses/models.py
@@ -0,0 +1,15 @@
+from application import db
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+    onupdate=db.func.current_timestamp())
+
+    name = db.Column(db.String(144), nullable=False)
+    description = db.Column(db.TEXT)
+    reservations = db.Column(db.Integer, nullable=False, default=0)
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
