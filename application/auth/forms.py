e93b58221edd760f0fc7741ab20f639ab9b15295
Sami <sami.rinksam@gmail.com>
2018-09-30 13:03:40 +0300

    Rekisteröityminen lisätty

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,10 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, validators
 
 
 class LoginForm(FlaskForm):
     username = StringField("Username")
     password = PasswordField("Password")
+
+    class Meta:
+        csrf = False
+
+
+class SignupForm(FlaskForm):
+    name = StringField("Name", [validators.InputRequired(), validators.length(min=2, message='Name too short')])
+    username = StringField("Username", [validators.InputRequired(), validators.length(min=2, message='Username too short')])
+    password = PasswordField("Password", [validators.InputRequired(),
+                                          validators.EqualTo('passwordConfirmation', message='Passwords must match')])
+    passwordConfirmation = PasswordField("Repeat Password", [validators.InputRequired()])
 
     class Meta:
         csrf = False
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,15 +1,16 @@
 from flask import render_template, request, redirect, url_for
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, current_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, SignupForm
+from flask import flash
 
 
-@app.route("/auth/login", methods = ["GET", "POST"])
+@app.route("/auth/login", methods=["GET", "POST"])
 def auth_login():
     if request.method == "GET":
-        return render_template("auth/loginform.html", form = LoginForm())
+        return render_template("auth/loginform.html", form=LoginForm())
 
     form = LoginForm(request.form)
     # mahdolliset validoinnit
@@ -25,4 +26,30 @@ def auth_login():
 @app.route("/auth/logout")
 def auth_logout():
     logout_user()
+    return redirect(url_for("index"))
+
+
+@app.route("/auth/signup", methods=["GET", "POST"])
+def auth_signup():
+    if not current_user:
+        return redirect(url_for("index"))
+
+    if request.method == "GET":
+        return render_template("auth/singupform.html", form=SignupForm())
+
+    form = SignupForm(request.form)
+
+    if not form.validate():
+        return render_template("auth/singupform.html", form=form)
+
+    if not User.query.filter_by(username=form.username.data).count() == 0:
+        return render_template("auth/singupform.html", form=form, error="Username not available")
+
+    u = User(form.name.data)
+    u.username = form.username.data
+    u.password = form.password.data
+
+    db.session().add(u)
+    db.session().commit()
+    flash('Account created')
     return redirect(url_for("index"))
